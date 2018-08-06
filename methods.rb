1d861f0ef44f003e8115beefa53e4b1430063eff
gabrielagsole <ggarciasole@gmail.com>
2018-08-06 20:32:00 +0000

    Done.

---
+++ methods.rb
@@ -0,0 +1,15 @@
+# def about_me
+#   puts "I am 16 years old"
+#   puts "I live in Barcelona"
+#   puts "My favourite TV show is Stranger Things"
+# end
+
+# about_me
+# about_me
+
+def greeting
+  local_greeting = "Bonjour"
+  puts "#{local_greeting}! I hope you had a great day!!!"
+end
+
+greeting
