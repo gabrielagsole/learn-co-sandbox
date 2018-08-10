7fb95df5596a3d10b787bd6baba990116bff8ffa
gabrielagsole <ggarciasole@gmail.com>
2018-08-10 18:22:51 +0000

    automatically backed up by learn

---
+++ clothes.rb
@@ -0,0 +1,10 @@
+tops = ["blouses", "t-shirts", "jackets"]
+bottoms = ["sandals", "sneakers", "shorts"]
+footwear = ["sandals", "sneakers", "heels"]
+
+clothes = [tops,bottoms,footwear]
+
+bottoms.push("overalls")
+
+puts "bottoms: #{bottoms}"
+puts "clothes: #{clothes}"
--- spirit.rb
+++ spirit.rb
@@ -1,14 +1,14 @@
-spirit_week = {
+def spirit
+  spirit_week = {
   :monday => "Pajama Day",
   :tuesday => "School Pride",
   :wednesday => "On Wednesdays we wear pink",
-  :tuesday => "Disney or movie character",
+  :thursday => "Disney or movie character",
   :friday => "Fancy Friday"
   
 }
-
-def spirit
 spirit_week.each do |key,value|
   puts "The spirit day for #{key} is #{value}"
 end 
+end  
 spirit 
